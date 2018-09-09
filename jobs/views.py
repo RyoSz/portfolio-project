16e737baf88a92396687dbcd65b70da6a42314cd
RyoSz <ryosuke.suzuki@outlook.com>
2018-09-09 10:03:55 +0900

    api added

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Job
+import requests
+import json
 
 def home(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content.decode('utf-8'))
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,EOS,BCH,DASH,ETC,XRP&tsyms=USD")
+    price = json.loads(price_request.content.decode('utf-8'))
+    return render(request, 'jobs/home.html', {'jobs':jobs, 'api':api, 'price':price})
+
+

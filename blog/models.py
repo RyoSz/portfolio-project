72cfdffb6f7df0d47f9b9ab7ee1960a58beff8cd
RyoSz <ryosuke.suzuki@outlook.com>
2018-12-09 14:09:34 +0900

    blog front end change

--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to="images/")
+    category = models.CharField(max_length=50, null=True, blank=True)
+    category_color = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.title
     
     def summary(self):
-        return self.body[:100]
+        return self.body[:25]
     
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %d %Y')
